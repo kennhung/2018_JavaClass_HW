f1f6e361618bda9e2f49071daf7f003aa8b09f16
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-09-30 17:45:59 +0800

    add ex7 for class2

---
+++ Example/Test/src/org/Team6083/EX7_Switch.java
@@ -0,0 +1,33 @@
+package org.Team6083;
+
+public class EX7_Switch {
+
+	public static void main(String[] args) {
+		int day = 1;
+
+		switch (day) {
+		case 1:
+			System.out.println("MON");
+			break;
+		case 2:
+			System.out.println("TUE");
+			break;
+		case 3:
+			System.out.println("WED");
+			break;
+		case 4:
+			System.out.println("THU");
+			break;
+		case 5:
+			System.out.println("FRI");
+			break;
+		case 6:
+		case 7:
+			System.out.println("政賢該減肥");
+			break;
+		default:
+
+		}
+	}
+
+}
