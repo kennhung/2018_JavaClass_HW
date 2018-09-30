48b4d7436e724d7f9d9de9d17f78fdc83820a717
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-09-30 18:19:56 +0800

    add Ex6

---
+++ Example/Test/src/org/Team6083/EX6_For.java
@@ -0,0 +1,12 @@
+package org.Team6083;
+
+public class EX6_For {
+	public static void main(String[] args) {
+		int a = 0;
+		int i;
+		for(i=0;a<=48763;i++) {
+			a = a+i;
+		}
+		System.out.println(i);
+	}
+}
